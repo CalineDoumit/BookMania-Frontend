8a803af714d168dc936152a6ab46a84304af23c5
Caline Doumit <calinedoumit@gmail.com>
2022-05-21 07:02:26 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import {HashRouter, BrowserRouter,Redirect, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const LoginForm = React.lazy(() => import("./pages/login/login"));
 const RegisterForm = React.lazy(() => import("./pages/register/register"));
+const HomePage = React.lazy(() => import("./pages/home/home"));
+const RatingForm = React.lazy(() => import("./pages/rating/rating"));
 
 function App() {
   return (
       <BrowserRouter>
           <Route path="/login"><LoginForm/></Route>
           <Route path="/register"><RegisterForm/></Route>
+          <Route path="/homepage"><HomePage/></Route>
+          <Route path="/rating"><RatingForm/></Route>
       </BrowserRouter>
   );
 }
--- src/utils/HTTPHelpers.js
+++ src/utils/HTTPHelpers.js
@@ -21,3 +21,21 @@ export function qPost(apiPath, body) {
     })
 
 }
+
+//Function to get from backend
+export function qGet(apiPath) {
+    return fetch(BackendURL + apiPath, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }).then(response => {
+            if (response.headers.get('content-length') === '0') {
+                return null;
+            }
+            return response.json();
+        }
+    )
+}
